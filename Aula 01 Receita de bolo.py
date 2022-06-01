c91a41d7dbf1d68dfac5ae8dfe71261719386a68
Fabricio2066 <106712793+Fabricio2066@users.noreply.github.com>
2022-06-01 20:56:59 -0300

    Add files via upload

---
+++ Aula 01 Receita de bolo.py
@@ -0,0 +1,91 @@
+# Algoritimo que ensina a fazer um bolo de chocolate
+# Feito por Fabricio no dia 01/06/2022 ás 20:18
+
+print("===========================================")
+print("")
+print("#----- Receita de bolo de chocolate------#")
+print("")
+print("===========================================")
+
+
+print("Ingredientes: ")
+
+print("""
+
+-> Massa
+
+3 ovos
+
+3 colheres (sopa) de manteiga sem sal
+
+1 xícara (chá) de açúcar
+
+2 xícaras (chá) de farinha de trigo peneirada
+
+1/2 xícara (chá) de chocolate em pó peneirado
+
+1 xícara (chá) de leite (240 ml)
+
+1 colher (sopa) de fermento químico
+
+1 colher (chá) de bicarbonato de sódio
+
+-> Calda
+
+3 xícaras (chá) de leite
+
+2 xícaras (chá) de chocolate em pó
+
+2 xícaras (chá) de açúcar
+
+2 colheres (sopa) de manteiga sem sal
+
+Cobertura
+
+2 latas de creme de leite sem soro
+
+1 e 1/2 xícara (chá) de açúcar
+
+1/2 xícara (chá) de chocolate em pó
+
+1 colher (sopa) de manteiga sem sal
+
+""")
+
+
+print("""
+
+-> Massa
+Bata os ovos com a manteiga e o açúcar na batedeira até esbranquiçar.
+
+Diminua a velocidade e acrescente a farinha, o chocolate e o leite.
+
+Adicione o fermento e o bicarbonato e bata rapidamente somente para incorporar.
+
+Despeje em fôrma retangular (34 x 22,5 cm) ou outra de sua preferência untada com manteiga e polvilhada com chocolate em pó.
+
+Leve ao forno médio preaquecido (180 °C) por cerca de 30 minutos ou até firmar.
+
+Deixe esfriar e corte em fatias.
+
+-> Calda
+
+Leve os ingredientes ao fogo até ferver. Utilize morna.
+
+-> Cobertura
+
+Misture os ingredientes em uma panela e leve ao fogo médio, mexendo sempre, até ficar consistente (uns 10 minutos).
+
+Transfira para uma tigela e leve à geladeira até esfriar.
+
+Montagem
+
+Em fôrma redonda (24 cm de diâmetro x 6 cm de altura) forrada com papel-alumínio, faça uma camada de bolo e regue com a calda.
+
+Repita as camadas, sempre regando com a calda, e pressione levemente com uma espátula.
+
+Cubra com papel-alumínio e leve à geladeira por 6 horas.
+
+Desenforme no prato de servir, espalhe a cobertura e decore a gosto (com raspas de chocolate, por exemplo).
+
+""")
