e1891b0bb6cf795df3f4f2a726fe6ac5fcb346e3
Fabricio2066 <106712793+Fabricio2066@users.noreply.github.com>
2022-06-01 22:44:24 -0300

    Add files via upload

---
+++ Aula 01 Sorve de Morango.py
@@ -0,0 +1,39 @@
+# Algoritimo que ensina a fazer um Sorvete de Morango 
+# Feito por Fabricio em 01/06/2022 ás 22:33
+
+
+print("----------------________________----------------")
+print("")
+print("======> Receita de Sorvete de Morango <======")
+print("")
+print("----------------________________----------------")
+
+print("")
+print("")
+
+print("Ingredientes: ")
+
+print("""
+
+1 Leite MOÇA® (lata ou caixinha) 395 g
+2 e meia xícaras (chá) de morangos picados
+2 Caixinhas de NESTLÉ® Creme de Leite
+1 colher (sopa) de suco de limão
+
+""")
+
+
+
+print("Modo de Preparo: ")
+
+print("""
+
+Em um liquidificador, bata o Leite MOÇA, 2 xícaras (chá) de morangos, o NESTLÉ Creme de Leite e o suco de limão até ficar homogêneo.
+
+Despeje em um recipiente refratário, cubra com plástico-filme e leve ao freezer por cerca de 4 horas.
+
+Retire, bata mais uma vez a mistura no liquidificador e acrescente meia xícara (chá) de morangos picados.
+
+Leve de volta ao freezer, por cerca de 2 horas ou até firmar.
+
+""")
