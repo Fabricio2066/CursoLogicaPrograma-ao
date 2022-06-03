f4576f85a6a2ceaef9fcc31216a75ed41fbff8f5
Fabricio2066 <106712793+Fabricio2066@users.noreply.github.com>
2022-06-03 20:00:05 -0300

    Add files via upload

---
+++ Aula 1 Linguas de programaçao usada para (D) (S) (J) (R).py
@@ -0,0 +1,17 @@
+print("As top línguas mais usadas para desktops\n")
+print("1- Java\n2- Kotlin\n3- Swift\n4- Objective C\n6- Python\n7- PHP\n8- Ruby\n9- HTML5\n10- C#")
+
+print("")
+
+print("As top línguas mais usadas para sites\n")
+print("1- Java\n2- JavaScript\n3- PHP\n4- Python\n5- Ruby\n6- HTML\n7- CSS")
+
+print("")
+
+print("As top línguas mais usadas para jogos")
+print("1- Python\n2- C\n3- C++\n4- C#\n5- Java\n6- JavaScript")
+
+print("")
+
+print("As línguas mais usadas para Robotica\n")
+print("1- Python\n2- C++\n3- JavaScript\n4- Java\n5- C#")
