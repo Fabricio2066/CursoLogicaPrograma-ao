fa95aec0f33eb2b1622df88132ec6ba589c18bd9
Fabricio2066 <106712793+Fabricio2066@users.noreply.github.com>
2022-06-02 21:11:00 -0300

    Add files via upload

---
+++ Aula 1 Trocar lampada.py
@@ -0,0 +1,8 @@
+# Algoritimo de como trocar uma lampada queimada
+
+print("Comprar uma lampada nova")
+print("Desligar a energia")
+print("Esperar a lampada esfriar")
+print("Retirar a lampada queimada")
+print("Colocar a lampada nova")
+print("Ligar a energia")
