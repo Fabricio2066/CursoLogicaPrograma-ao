bdd0f94a66286a07669c49dc7014a19f497df85f
Fabricio2066 <106712793+Fabricio2066@users.noreply.github.com>
2022-06-02 21:10:45 -0300

    Add files via upload

---
+++ Aula 1 Trocar pneu,.py
@@ -0,0 +1,25 @@
+# Algoritimo de como trocar Pneu
+
+print("Pegar step, chave de roda e o macaco")
+
+print("Colocar o macaco debaixo do carro")
+
+print("Operar o macaco, sem levantar a roda")
+
+print("Remover os parafusos e a roda")
+
+print("Colocar o step")
+
+print("Colocar os parafusos sem muita força")
+
+print("Operar o macaco em sentindo \"repouso\" ")
+
+print("Remover o macaco debaixo do carro")
+
+print("Aperta os parafusos da roda com um toque aceitavel")
+
+print("Conferir")
+
+print("")
+
+print("FIM")
