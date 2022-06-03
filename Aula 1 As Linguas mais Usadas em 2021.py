dd3a1dc998e135f7d1d44e3793e00668870e79c2
Fabricio2066 <106712793+Fabricio2066@users.noreply.github.com>
2022-06-03 19:21:52 -0300

    Add files via upload

---
+++ Aula 1 As Linguas mais Usadas em 2021.py
@@ -0,0 +1,18 @@
+# Algoritimo sobre as 10 Línguas de Programação mais usadas em 2021
+
+print("")
+
+print("AS 10 LINGUAS MAIS USADAS EM 2021")
+
+print("")
+
+print("1- C")
+print("2- Python")
+print("3- Java")
+print("4- C++")
+print("5- C#")
+print("6- Visual basic")
+print("7- JavaScript")
+print("8- PHP")
+print("9- R")
+print("10- SQL")
