3f45a2e8975f4e1116205b66b3bbd8d520b167e7
Fabricio2066 <106712793+Fabricio2066@users.noreply.github.com>
2022-07-07 15:13:10 -0300

    Add files via upload

---
+++ Aula 02/Aula 2.1 .py
@@ -0,0 +1,12 @@
+
+NomeCliente = "João da Silva"
+
+
+
+print("Olá Sr(a)", NomeCliente)
+#....
+print("Prezado Sr(a)", NomeCliente,", estamos...")
+#....
+print("Sr(a)", NomeCliente ,", seu ...")
+#....
+print("Até mais Sr(a)", NomeCliente," :)")
