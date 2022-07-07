7712e419781fbefa05ee1e6d91b72ec335fe0d15
Fabricio2066 <106712793+Fabricio2066@users.noreply.github.com>
2022-07-07 15:17:04 -0300

    Add files via upload

---
+++ Aula 02/Exercicio 2.4.py
@@ -0,0 +1,8 @@
+
+NomeCliente = input("Digite o seu nome pf\n")
+IdadeCliente = input("Digite sua idade\n")
+
+print("O seu nome é:", NomeCliente, 'e sua idade é', IdadeCliente)
+
+
+# o imput() so traz texto
