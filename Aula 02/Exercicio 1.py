8c28b43c63be00997b8c7f62d82489a28cdc7f89
Fabricio2066 <106712793+Fabricio2066@users.noreply.github.com>
2022-07-07 15:11:32 -0300

    Add files via upload

---
+++ Aula 02/Exercicio 1.py
@@ -0,0 +1,9 @@
+# Nome, Idade, Altura depois repita tudo na tela 
+
+
+Nome = input('Digite seu nome: ')
+Idade = int( input("Digite sua idade: "))
+Altura = float(input("Digete sua altura: "))
+
+
+print("O nome do Sr(a) é:", Nome, " Sua idade é:", Idade, "Sua altura é: ", Altura)
