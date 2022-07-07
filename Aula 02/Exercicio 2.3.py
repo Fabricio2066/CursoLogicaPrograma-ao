c7e93a19a072410d79976edf8ffb89f90e6c2b91
Fabricio2066 <106712793+Fabricio2066@users.noreply.github.com>
2022-07-07 15:16:49 -0300

    Add files via upload

---
+++ Aula 02/Exercicio 2.3.py
@@ -0,0 +1,18 @@
+
+Variavel_1 = "Igor"
+Variavel_2 = "50"
+Variavel_3 = 50
+Variavel_4 = 50.3 
+Varivael_5 = True 
+Variavel_6 = False
+
+
+Variavel_7 = 50,3
+
+print("O nome é texto(String) e em python chamamos de:", type(Variavel_1) )
+print("O num com aspas é texto(String) e em python chamamos de:", type(Variavel_2) )
+print("O num sem . é inteiro(integer) e em python chamamos de: ", type(Variavel_3) )
+print("O num com . é flutuante(float) e em python chamamos de:", type(Variavel_4) )
+print("O logico boolean(bool) e em python chamamos de:", type(Varivael_5) )
+print("O logico boolean(bool) e em python chamamos de:", type(Variavel_6) )
+print("O num com , é ?(?) e em python chamamos de:", type(Variavel_7) )
