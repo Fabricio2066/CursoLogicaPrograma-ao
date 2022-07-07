885fcf9def6d39494292b811fd8274e49de87693
Fabricio2066 <106712793+Fabricio2066@users.noreply.github.com>
2022-07-07 15:12:48 -0300

    Add files via upload

---
+++ Aula 02/Exercicio 5.py
@@ -0,0 +1,9 @@
+# Peça um salario, e multiplique por 1.22
+
+Salario = float(input('Coloque o valor do seu salario: '))
+
+Aumento = float(input("Digite o aumento: "))
+
+print("\n")
+
+print("O seu salario aumentou em", Aumento, ", e o salario atual é de: ", Salario*Aumento)
