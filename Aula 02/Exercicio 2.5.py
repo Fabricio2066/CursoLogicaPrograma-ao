78587e9d9309fde44e65f2866bf5d6232b91110f
Fabricio2066 <106712793+Fabricio2066@users.noreply.github.com>
2022-07-07 15:17:30 -0300

    Add files via upload

---
+++ Aula 02/Exercicio 2.5.py
@@ -0,0 +1,11 @@
+
+num1 = int( input('Digite um número: '))
+num2 = int( input('Digite um número: '))
+
+
+resp = num1 + num2
+
+
+print("A resposta da soma é", resp)
+
+#int()       float()       str()       bool()
