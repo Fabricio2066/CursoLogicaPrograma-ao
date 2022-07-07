8434cb8cb15984c8da662a4f93fd0f1531db37d9
Fabricio2066 <106712793+Fabricio2066@users.noreply.github.com>
2022-07-07 15:12:26 -0300

    Add files via upload

---
+++ Aula 02/Exercicio 4.py
@@ -0,0 +1,14 @@
+# Peça 4 notas de um aluno e calcule a media
+
+NomeAluno = input("Digite o nome: ")
+
+Nota1 = float( input("Digite a nota: "))
+Nota2 = float( input("Digite a nota: "))
+Nota3 = float( input("Digite a nota: "))
+Nota4 = float( input("Digite a nota: "))
+
+Resultado = float( Nota1 + Nota2 + Nota3 + Nota4)
+
+print("\n")
+
+print("A media do aluno(a)", NomeAluno, "é:", Resultado / 4)
