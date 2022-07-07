6d7d1cb8091b5cff116c5655d7ed1337b0714b7e
Fabricio2066 <106712793+Fabricio2066@users.noreply.github.com>
2022-07-07 15:12:08 -0300

    Add files via upload

---
+++ Aula 02/Exercicio 3.py
@@ -0,0 +1,13 @@
+# Dois numeros, depois escreva a soma e multiplicaçao na tela 
+
+
+Num1 = int( input("Digite um numero: "))
+Num2 = int( input("Digite um numero: "))
+
+print("\n")
+
+print("A soma de", Num1, "mais", Num2,"tem como resultado", Num1+Num2)
+
+print("\n")
+
+print("A multiplicaçao de", Num1, "vezes", Num2, "tem como resultado", Num1*Num2)
