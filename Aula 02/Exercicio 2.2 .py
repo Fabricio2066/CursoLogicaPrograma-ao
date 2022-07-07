064018a75d537974dba23001742a6b6bdd54dd97
Fabricio2066 <106712793+Fabricio2066@users.noreply.github.com>
2022-07-07 15:16:35 -0300

    Add files via upload

---
+++ Aula 02/Exercicio 2.2 .py
@@ -0,0 +1,11 @@
+#Aplicativo apresenta o programador 
+
+Nome = "Igor"
+Idade = 22
+Altura = 1.23
+isProfessor = True 
+
+print("Olá o seu nome é:", Nome)
+print("Sua idade é", Idade)
+print("Sua altura é", Altura)
+print("Você é professor?", isProfessor)
