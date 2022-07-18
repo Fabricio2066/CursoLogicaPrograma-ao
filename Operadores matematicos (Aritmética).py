700f1be9de48bb56f282f0520b5a4445f229d3aa
Fabricio2066 <106712793+Fabricio2066@users.noreply.github.com>
2022-07-18 19:40:24 -0300

    Add files via upload

---
+++ Operadores matematicos (Aritmética).py
@@ -0,0 +1,21 @@
+#Operadores matemático (Aritmética)
+
+# Soma           +
+# Subtração      -
+# Multiplicação  *
+# Divisão        /
+# Resto Divisão  %
+# Porcetagem     (Digitar em formato decimal!)
+
+print(50*'=')
+print("Calculadora")
+print(50*'=')
+print("")
+
+numero1 = float( input("Digite um numero: "))
+numero2 = float( input("Digite um numero: "))
+
+print('A soma dos valores é: ', numero1+numero2 )
+print('A subtração dos valores é: ', numero1-numero2 )
+print('A multiplicação dos valores é: ', numero1*numero2 )
+print('A divisão dos valores é: ', numero1/numero2 )
