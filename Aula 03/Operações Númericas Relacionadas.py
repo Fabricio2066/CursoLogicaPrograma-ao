9022e87546a7a74ca532b4bdbd32e9f46e6bc592
Fabricio2066 <106712793+Fabricio2066@users.noreply.github.com>
2022-07-18 19:41:38 -0300

    Add files via upload

---
+++ Aula 03/Operações Númericas Relacionadas.py
@@ -0,0 +1,12 @@
+#Operações Númericas Relacionadas
+
+# Maior que (>),
+# Menor que (<),
+# Igual (==),
+# Maior ou Igual (>=),
+# Menor ou Igual (<=),
+# Diferente (!=)
+
+idade = int( input("Digite sua idade: "))
+
+print("Já tem 20 anos? ", idade >= 20 )
