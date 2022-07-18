b4aa36f6f81a5d9afa6755536642f671dd1f3cb7
Fabricio2066 <106712793+Fabricio2066@users.noreply.github.com>
2022-07-18 19:42:03 -0300

    Add files via upload

---
+++ Aula 03/App Saber se é maior de idade.py
@@ -0,0 +1,9 @@
+idade = int( input("Digite sua idade: "))
+
+if idade >= 18:
+    print('Você já é maior de idade, e já pode dirigir!')
+else:
+    print('Você AINDA NÃO é maior de idade, e NÃO pode dirigir!')
+    
+
+print('Bye!')
