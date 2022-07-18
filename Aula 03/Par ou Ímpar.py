ee68a903f47dd5cfe60dbfa05dd1bd97f224f1d3
Fabricio2066 <106712793+Fabricio2066@users.noreply.github.com>
2022-07-18 19:41:22 -0300

    Add files via upload

---
+++ Aula 03/Par ou Ímpar.py
@@ -0,0 +1,14 @@
+# Par ou Ímpar
+
+#Qual valor dividido por 2 que O RESTO É 0 é Par
+#Qual valor dividido por 2 que O RESTO É 1 é Ímpar
+
+
+# Multiplo 
+
+# 3 é Multiplo de 27 
+
+# Primeiro teste sempre o valor MAIOR % valor MENOR
+# Ver se é igual a ZERO (Multiplo), qual valor NÃO É Multiplo
+
+print(  80 % 5 )
