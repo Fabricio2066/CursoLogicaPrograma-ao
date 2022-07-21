1bf1e5b3df5cd28649ce5fa818170b3fbbf00bc3
Fabricio2066 <106712793+Fabricio2066@users.noreply.github.com>
2022-07-21 13:09:03 -0300

    Add files via upload

---
+++ Aula 4/#ENQUANTO TESTE_VERDADEIRO.py
@@ -0,0 +1,10 @@
+#ENQUANTO TESTE_VERDADEIRO:
+#    LINHA DE CODIGO 
+
+Contador = 1 
+
+while Contador <= 10: 
+    print(f'Estamos na {Contador}ª execução! ')
+    Contador = Contador + 1 
+    
+print('bye')
